fb108ec3d042d557db868575df759af95b4a4ad6
João Ricardo Czeszak Brançani <jori2710@gmail.com>
2021-08-18 18:37:58 -0300

    Versão inicial do site Bikcraft

---
+++ js/script.js
@@ -0,0 +1,18 @@
+if(window.SimpleSlide) { 
+new SimpleSlide({
+    slide: "quote",
+    nav: false,
+    time: 5000 // tempo de transição dos slides
+}) 
+
+new SimpleSlide({
+    slide: "portfolio",
+    nav: true,
+    time: 5000
+})
+
+}
+
+if(window.SimpleAnime) {
+new SimpleAnime();
+}
